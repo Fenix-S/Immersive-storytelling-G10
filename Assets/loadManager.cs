5b189e7d41ed9b0e028e2ac3effaebd4b3839b15
Kobe Bogaerts <kobe.bogaerts@student.ap.be>
2020-12-07 13:05:00 +0100

    added loading to universe scene

---
+++ Assets/loadManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadManager : MonoBehaviour
+{
+    private bool hasFallen = false;
+
+    void OnTriggerEnter(Collider e)
+    {
+        if (!hasFallen && e.tag == "Player")
+        {
+            StartCoroutine(LoadUniverseScene());
+            hasFallen = true;
+        }
+    }
+
+    IEnumerator LoadUniverseScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(2);
+    }
+
+}
