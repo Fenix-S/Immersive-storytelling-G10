2e93268a91816841410fe28933e4eec77b5a7d6a
Fenix-S <elias.tobback@student.ap.be>
2020-12-12 12:45:07 +0100

    Bunch of fixes from testing: size,transition, controllers...

---
+++ Assets/Scripts/bedroomToStairs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bedroomToStairs : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider e)
+    {
+        if (e.tag.Equals("Player"))
+        {
+            StartCoroutine(LoadStairsScene());
+        }
+    }
+
+    IEnumerator LoadStairsScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(1);
+    }
+
+}
